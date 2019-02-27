667b48fa0f1d49d5595d32c59952ca9d0d9f9745
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-02-27 18:53:44 -0500

    setup of data and graph

---
+++ A2/main.py
@@ -0,0 +1,35 @@
+from pandas import read_csv
+from graph import Graph
+from time import time
+
+POP_SIZE = 100
+
+def setup_graph(df, cities) -> Graph:
+    g = Graph()
+    for v in cities:
+        g.add_vertex(v)
+    for v0 in range(0, len(cities)):
+        for v1 in range(v0+1, len(cities)):
+            g.add_edge(cities[v0], cities[v1], df[cities[v0]][v1])
+    return g
+
+def main():
+    start = time()
+    start_setup = time()
+    df = read_csv('data_set1.csv', index_col=False)
+    cities = list(df.columns)[1:]
+    g = setup_graph(df, cities)
+    end_setup = time()
+    
+    # Prints all the relations in the graph
+    # for v in g:
+    #     for w in v.get_connections():
+    #         vid = v.get_id()
+    #         wid = w.get_id()
+    #         print(f'( {vid} , {wid}, {v.get_weight(w)})')
+    end = time()
+    print(f'Setup Runtime: {end_setup - start_setup}')
+    print(f'Total Runtime: {end - start}')
+
+if __name__ == '__main__':
+    main()
