02cbddb4e54b1172f96e3975d81ed66246f7a8d0
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-20 20:53:07 -0400

    refactoring

--- data_structs/graph.py
+++ data_structs/graph.py
@@ -29,33 +29,3 @@ class Graph:
 
     def get_vertices(self):
         return self.vert_dict.keys()
-
-# if __name__ == '__main__':
-
-#     g = Graph()
-
-#     g.add_vertex('a')
-#     g.add_vertex('b')
-#     g.add_vertex('c')
-#     g.add_vertex('d')
-#     g.add_vertex('e')
-#     g.add_vertex('f')
-
-#     g.add_edge('a', 'b', 7)
-#     g.add_edge('a', 'c', 9)
-#     g.add_edge('a', 'f', 14)
-#     g.add_edge('b', 'c', 10)
-#     g.add_edge('b', 'd', 15)
-#     g.add_edge('c', 'd', 11)
-#     g.add_edge('c', 'f', 2)
-#     g.add_edge('d', 'e', 6)
-#     g.add_edge('e', 'f', 9)
-
-#     for v in g:
-#         for w in v.get_connections():
-#             vid = v.get_id()
-#             wid = w.get_id()
-#             print(f'( {vid} , {wid}, {v.get_weight(w)})')
-
-#     for v in g:
-#         print(f'g.vert_dict[{v.get_id()}]={g.vert_dict[v.get_id()]}')
